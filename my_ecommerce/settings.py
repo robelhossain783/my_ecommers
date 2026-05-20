9252ff2d9cf518bb487eb16fbee06a874f0b8adc
Robel Hossain <robel.hossain@upaybd.com>
2026-05-20 23:21:08 +0600

    settigs  issue solve and fix issues

--- my_ecommerce/settings.py
+++ my_ecommerce/settings.py
@@ -45,11 +45,13 @@ THIRD_PARTY_APPS = [
     'product',
     'cart',
     'order',
+    'corsheaders',
 ]
 
 INSTALLED_APPS = SYSTEM_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,7 +134,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+CORS_ALLOW_ALL_ORIGINS = True
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
