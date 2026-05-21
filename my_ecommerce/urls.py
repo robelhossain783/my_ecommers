0d100730796d86d8dd2f6994b2323e007afc2d97
Robel Hossain <robel.hossain@upaybd.com>
2026-05-22 02:04:14 +0600

    fix media url

--- my_ecommerce/urls.py
+++ my_ecommerce/urls.py
@@ -13,18 +13,54 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.conf import settings
+# from django.conf.urls.static import static
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/', include('product.urls')),
+#
+#     path('api/', include('cart.urls')),
+#
+#     path('api/', include('order.urls')),
+# ]
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # APIs
     path('api/', include('product.urls')),
-
     path('api/', include('cart.urls')),
-
     path('api/', include('order.urls')),
 ]
+
+# =========================
+# LOCAL MEDIA (DEBUG ONLY)
+# =========================
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+
+# =========================
+# PRODUCTION MEDIA (Render FIX)
+# =========================
+urlpatterns += [
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT}
+    ),
+]
