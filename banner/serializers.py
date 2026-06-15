45c51b5fe68c350877fcf16ae8279b10a811ea83
Robel Hossain <robel.hossain@upaybd.com>
2026-06-15 12:22:12 +0600

    fix: make notification banner image writable and enforce secure cloudinary storage

--- banner/serializers.py
+++ banner/serializers.py
@@ -11,8 +11,6 @@ class BannerSerializer(serializers.ModelSerializer):
 #  Notify banner serializer
 class NotificationBannerSerializer(serializers.ModelSerializer):
 
-    image = serializers.SerializerMethodField()
-
     class Meta:
         model = NotificationBanner
         fields = (
@@ -25,15 +23,3 @@ class NotificationBannerSerializer(serializers.ModelSerializer):
             "end_date",
             "created_at",
         )
-
-    def get_image(self, obj):
-        if not obj.image:
-            return None
-
-        request = self.context.get("request")
-
-        if request is not None:
-            return request.build_absolute_uri(obj.image.url)
-
-        # fallback: return relative URL if no request context
-        return obj.image.url
